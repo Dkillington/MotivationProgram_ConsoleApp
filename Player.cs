a5c084534a3821918906c966e0feac85728826e5
David K <Dkillian00@yahoo.com>
2024-06-23 16:51:09 -0400

    First Commit

---
+++ Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MotivationProgram
+{
+    [Serializable]
+    class Player
+    {
+        public int timesPlayed { get; set; }
+        public int timesAsked { get; set; }
+        public int timesAskedThisSession { get; set; }
+        public string name { get; set; }
+        public int mood { get; set; }
+        public bool bff { get; set; }
+
+        //-1 Bad
+        //0 Neutral
+        //1 Good
+    }
+}
